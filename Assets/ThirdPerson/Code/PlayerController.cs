0eab21afec331278f798b117172223c6a4287a77
Sourav Chatterjee <sromic@gmail.com>
2021-10-04 14:57:32 +0530

    Basic animation setup for characfer controller

---
+++ Assets/ThirdPerson/Code/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2;
+    private Vector2 _moveDirection;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>();
+    }
+
+    private void Move(Vector2 direction)
+    {
+        transform.Translate(direction.x * moveSpeed * Time.deltaTime, 0, direction.y * moveSpeed * Time.deltaTime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(_moveDirection);
+    }
+}
