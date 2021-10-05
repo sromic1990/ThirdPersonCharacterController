da0ca74618af2a4ae50e47fdc6c607788c092e56
Sourav Chatterjee <sromic@gmail.com>
2021-10-05 14:06:14 +0530

    Gun Attach and detach experiment with rifle and character. Code cleanup started.

--- Assets/ThirdPerson/Code/PlayerController.cs
+++ Assets/ThirdPerson/Code/PlayerController.cs
@@ -1,157 +1,182 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace ThirdPerson.Code
 {
-    #region FIELDS AND MEMBER VARIABLES
-    #region SERIALIZED MEMBERS
-    [SerializeField] private float moveSpeed = 2.0f;
-    [SerializeField] private float maxSpeed = 10f;
-    [SerializeField] private float turnSpeed = 100f;
-    [SerializeField] private float groundAcceleration = 5.0f;
-    [SerializeField] private float groundDeceleration = 25.0f;
-    [SerializeField] private float jumpSpeed = 30000f;
-    [SerializeField] private float groundRayDist = 2.0f;
-    #endregion
-    
-    #region FLOAT MEMBERS
-    private Vector2 _moveDirection;
-    private float _jumpDirection; 
-    private float _desiredSpeed;
-    private float _forwardSpeed;
-
-    private float _jumpEffort = 0.0f;
-    #endregion
-
-    #region UNITY COMPONENT CACHES
-    private Animator _anim;
-    private Rigidbody _rigidBody;
-    #endregion
-
-    #region BOOL MEMBERS
-    private bool _onGround = true;
-    private bool _readyJump = false;
-    #endregion
-
-    #region ANIMATOR HASHES
-    private readonly int _forwardSpeedHash = Animator.StringToHash("Speed");
-    private readonly int _jumpBoolHash = Animator.StringToHash("ReadyJump");
-    private readonly int _launchBoolHash = Animator.StringToHash("Launch");
-    private readonly int _landBoolHash = Animator.StringToHash("Land");
-    private readonly int _landVelocityHash = Animator.StringToHash("LandingVelocity");
-    private readonly int _fallingBoolHash = Animator.StringToHash("Falling");
-    #endregion
-    
-    #region PROPERTIES
-    private bool IsMoveInput => !Mathf.Approximately(_moveDirection.sqrMagnitude, 0f);
-    #endregion
-    
-    #endregion
-
-    #region METHODS
-    #region UNITY EVENT METHODS
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        _anim = GetComponent<Animator>();
-        _rigidBody = GetComponent<Rigidbody>();
-    }
+        #region FIELDS AND MEMBER VARIABLES
 
-    private void Update()
-    {
-        Move(_moveDirection);
-        Jump(_jumpDirection);
-        CheckLanding();
-    }
-    #endregion
+        #region SERIALIZED MEMBERS
 
-    #region INPUT SYSTEM EVENTS
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        _moveDirection = context.ReadValue<Vector2>();
-    }
+        [SerializeField] private float moveSpeed = 2.0f;
+        [SerializeField] private float maxSpeed = 10f;
+        [SerializeField] private float turnSpeed = 100f;
+        [SerializeField] private float groundAcceleration = 5.0f;
+        [SerializeField] private float groundDeceleration = 25.0f;
+        [SerializeField] private float jumpSpeed = 30000f;
+        [SerializeField] private float groundRayDist = 2.0f;
 
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        _jumpDirection = context.ReadValue<float>();
-    }
-    #endregion
+        #endregion
 
-    #region PRIVATE METHODS
-    private void Move(Vector2 direction)
-    {
-        float turnAmount = direction.x;
-        float fDirection = direction.y;
-        
-        if (direction.sqrMagnitude > 1)
+        #region FLOAT MEMBERS
+
+        private Vector2 _moveDirection;
+        private float _jumpDirection;
+        private float _desiredSpeed;
+        private float _forwardSpeed;
+
+        private float _jumpEffort = 0.0f;
+
+        #endregion
+
+        #region UNITY COMPONENT CACHES
+
+        private Animator _anim;
+        private Rigidbody _rigidBody;
+
+        #endregion
+
+        #region BOOL MEMBERS
+
+        private bool _onGround = true;
+        private bool _readyJump = false;
+
+        #endregion
+
+        #region ANIMATOR HASHES
+
+        private readonly int _forwardSpeedHash = Animator.StringToHash("Speed");
+        private readonly int _jumpBoolHash = Animator.StringToHash("ReadyJump");
+        private readonly int _launchBoolHash = Animator.StringToHash("Launch");
+        private readonly int _landBoolHash = Animator.StringToHash("Land");
+        private readonly int _landVelocityHash = Animator.StringToHash("LandingVelocity");
+        private readonly int _fallingBoolHash = Animator.StringToHash("Falling");
+
+        #endregion
+
+        #region PROPERTIES
+
+        private bool IsMoveInput => !Mathf.Approximately(_moveDirection.sqrMagnitude, 0f);
+
+        #endregion
+
+        #endregion
+
+        #region METHODS
+
+        #region UNITY EVENT METHODS
+
+        private void Awake()
         {
-            direction.Normalize();
+            _anim = GetComponent<Animator>();
+            _rigidBody = GetComponent<Rigidbody>();
         }
 
-        _desiredSpeed = direction.magnitude * maxSpeed * Mathf.Sign(fDirection);
-        float acceleration = IsMoveInput ? groundAcceleration : groundDeceleration;
+        private void Update()
+        {
+            Move(_moveDirection);
+            Jump(_jumpDirection);
+            CheckLanding();
+        }
 
-        _forwardSpeed = Mathf.MoveTowards(_forwardSpeed, _desiredSpeed, acceleration * Time.deltaTime);
-        _anim.SetFloat(_forwardSpeedHash, _forwardSpeed);
-        
-        transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
-    }
-    
-    private void Jump(float direction)
-    {
-        if (direction > 0 && _onGround)
+        #endregion
+
+        #region INPUT SYSTEM EVENTS
+
+        public void OnMove(InputAction.CallbackContext context)
         {
-            _anim.SetBool(_jumpBoolHash, true);
-            _readyJump = true;
-            _jumpEffort += Time.deltaTime;
+            _moveDirection = context.ReadValue<Vector2>();
         }
-        else if (_readyJump)
+
+        public void OnJump(InputAction.CallbackContext context)
         {
-            _anim.SetBool(_launchBoolHash, true);
-            _readyJump = false;
-            _anim.SetBool(_jumpBoolHash, false);
+            _jumpDirection = context.ReadValue<float>();
         }
-    }
-    
-    private void CheckLanding()
-    {
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position + Vector3.up * groundRayDist * 0.5f, -Vector3.up);
-        Debug.DrawRay(transform.position + Vector3.up * groundRayDist * 0.5f, -Vector3.up);
-        if (Physics.Raycast(ray, out hit, groundRayDist))
+
+        #endregion
+
+        #region PRIVATE METHODS
+
+        private void Move(Vector2 direction)
         {
-            if (!_onGround)
+            float turnAmount = direction.x;
+            float fDirection = direction.y;
+
+            if (direction.sqrMagnitude > 1)
             {
-                _onGround = true;
-                _anim.SetFloat(_landVelocityHash, _rigidBody.velocity.magnitude);
-                _anim.SetBool(_landBoolHash, true);
-                _anim.SetBool(_fallingBoolHash, false);
+                direction.Normalize();
+            }
+
+            _desiredSpeed = direction.magnitude * maxSpeed * Mathf.Sign(fDirection);
+            float acceleration = IsMoveInput ? groundAcceleration : groundDeceleration;
+
+            _forwardSpeed = Mathf.MoveTowards(_forwardSpeed, _desiredSpeed, acceleration * Time.deltaTime);
+            _anim.SetFloat(_forwardSpeedHash, _forwardSpeed);
+
+            transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+        }
+
+        private void Jump(float direction)
+        {
+            if (direction > 0 && _onGround)
+            {
+                _anim.SetBool(_jumpBoolHash, true);
+                _readyJump = true;
+                _jumpEffort += Time.deltaTime;
+            }
+            else if (_readyJump)
+            {
+                _anim.SetBool(_launchBoolHash, true);
+                _readyJump = false;
+                _anim.SetBool(_jumpBoolHash, false);
             }
         }
-        else
+
+        private void CheckLanding()
         {
-            _onGround = false;
-            _anim.SetBool(_fallingBoolHash, true);
-            _anim.applyRootMotion = false;
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position + Vector3.up * groundRayDist * 0.5f, -Vector3.up);
+            Debug.DrawRay(transform.position + Vector3.up * groundRayDist * 0.5f, -Vector3.up);
+            if (Physics.Raycast(ray, out hit, groundRayDist))
+            {
+                if (!_onGround)
+                {
+                    _onGround = true;
+                    _anim.SetFloat(_landVelocityHash, _rigidBody.velocity.magnitude);
+                    _anim.SetBool(_landBoolHash, true);
+                    _anim.SetBool(_fallingBoolHash, false);
+                }
+            }
+            else
+            {
+                _onGround = false;
+                _anim.SetBool(_fallingBoolHash, true);
+                _anim.applyRootMotion = false;
+            }
         }
-    }
-    #endregion
 
-    #region ANIMATION DRIVEN CODE
-    public void Launch()
-    {
-        _anim.applyRootMotion = false;
-        _rigidBody.AddForce(0, jumpSpeed * Mathf.Clamp(_jumpEffort, 1, 3), 0);
-        _anim.SetBool(_launchBoolHash, false);
-    }
+        #endregion
 
-    public void Land()
-    {
-        _anim.SetBool(_landBoolHash, false);
-        _anim.applyRootMotion = true;
-        _anim.SetBool(_launchBoolHash, false);
-        _jumpEffort = 0;
+        #region ANIMATION DRIVEN CODE
+
+        public void Launch()
+        {
+            _anim.applyRootMotion = false;
+            _rigidBody.AddForce(0, jumpSpeed * Mathf.Clamp(_jumpEffort, 1, 3), 0);
+            _anim.SetBool(_launchBoolHash, false);
+        }
+
+        public void Land()
+        {
+            _anim.SetBool(_landBoolHash, false);
+            _anim.applyRootMotion = true;
+            _anim.SetBool(_launchBoolHash, false);
+            _jumpEffort = 0;
+        }
+
+        #endregion
+
+        #endregion
     }
-    #endregion
-    
-    #endregion
 }
---
+++ Assets/ThirdPerson/Code/PlayerWeaponController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace ThirdPerson.Code
+{
+    public class PlayerWeaponController : MonoBehaviour
+    {
+        [SerializeField] private Transform weapon;
+        [SerializeField] private Transform hand;
+        [SerializeField] private Transform hip;
+        
+        public void PickupGun()
+        {
+            weapon.SetParent(hand);
+            weapon.localPosition = new Vector3(0.054f, -0.029f, -0.011f);
+            weapon.localRotation = Quaternion.Euler(new Vector3(-20.26f, 76.985f, 125.45f));
+            weapon.localScale = Vector3.one;
+        }
+
+        public void PutDownGun()
+        {
+            weapon.SetParent(hip);
+            weapon.localPosition = new Vector3(0.0853f, 0.0509f, -0.0775f);
+            weapon.localRotation = Quaternion.Euler(new Vector3(94.578f, 187.022f, 172.451f));
+            weapon.localScale = Vector3.one;
+        }
+    }
+}
